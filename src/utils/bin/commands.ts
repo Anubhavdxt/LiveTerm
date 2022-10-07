1871629be5239adfd820a2a1b145aef5895528bf
Anubhav <anubhavdxt46@gmail.com>
2022-10-07 23:44:24 +0530

    Increase the number of commands in a single line

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -8,7 +8,7 @@ export const help = async (args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
   var c = '';
   for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
-    if (i % 7 === 0) {
+    if (i % 11 === 0) {
       c += Object.keys(bin).sort()[i - 1] + '\n';
     } else {
       c += Object.keys(bin).sort()[i - 1] + ' ';
